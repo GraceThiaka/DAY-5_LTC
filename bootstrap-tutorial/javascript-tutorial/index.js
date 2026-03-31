7dd3b1c5a44ea91ee260945a6548cbc4493b838c
GraceThiaka <thiakagracey@gmmal.com>
2026-03-31 14:18:34 +0300

    added new files

--- bootstrap-tutorial/javascript-tutorial/index.js
+++ bootstrap-tutorial/javascript-tutorial/index.js
@@ -28,10 +28,12 @@
 // r//esult = Number(num1)+Number(num2);
 // //console.log("your result is:",result)
 
-let button = document.getElementById("button");
+// let button = document.getElementById("button");
 
-button.addEventListener("click",function()
-{
+// button.addEventListener("click",function){
+    
+// }
+
 let num1 = document.getElementById("num1").value
 let num2 = document.getElementById("num2").value
 
@@ -39,8 +41,41 @@ let result = Number(num1)+Number(num2);
 console.log("Your result is;",result);
 let output =document.getElementById("results");
 output.innerText ="your result is:"+result;
-})
 
+// })
+// let a =20
+// let b =40
+// if(true){
+//     console.log("a is greater than b")
+// }
+// else if(false){
+//     console.log("a is less than b")
+// }
+for(let i=0;i<11;i++){
+//     console.log(i)
+// }
+// let counter=20
+// while(counter<30){
+//     counter++
+//     console.log("loops are amazing")
+// }
+// let x=35;
+// do{
+//     console.log("Roleen is sleeping");
+//     x++
+// }
+// while(x<30)
+
+//     fruits=["apple", "banana", "orange"]
+//     for (let fruit of fruits){
+//         console.log|(fruit)
+function addNumbers(a,b){
+    let sum =a+b;
+    console.log(sum)
+}
+add
+        
+    }
 
 // console.log(typeof(secondName));
 // console.log(typeof(pie));
