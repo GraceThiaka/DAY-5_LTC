df9a57410f88964153cd44a1bb105b0adaaf5d6e
GraceThiaka <thiakagracey@gmmal.com>
2026-04-02 14:06:31 +0300

    data_structures

---
+++ data_structures/index.js
@@ -0,0 +1,22 @@
+let fruits = ["apple","mango","pineapple"];
+console.log(fruits)
+fruits[1] = "avocado"
+console.log(fruits)
+fruits.push("banana")
+console.log(fruits)
+fruits.pop()
+console.log(fruits)
+//looping through arrays
+fruits.forEach((fruit) => {
+
+}
+
+)
+    console.log(fruit)
+let numbers =b [1,2,3,4,5,6,7,8,9,10];
+let evennembers = numbers.filter((number)=>{
+    return number% 2===0;
+
+}
+
+)
