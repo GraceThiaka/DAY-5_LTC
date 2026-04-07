f1f1f4915e754fadf80c1c17107edf30e3aafa2d
GraceThiaka <thiakagracey@gmmal.com>
2026-04-07 14:10:00 +0300

    arrays

--- data_structures/index.js
+++ data_structures/index.js
@@ -1,22 +1,32 @@
-let fruits = ["apple","mango","pineapple"];
-console.log(fruits)
-fruits[1] = "avocado"
-console.log(fruits)
-fruits.push("banana")
-console.log(fruits)
-fruits.pop()
-console.log(fruits)
-//looping through arrays
-fruits.forEach((fruit) => {
+// let fruits = ["apple","mango","pineapple"];
+// console.log(fruits)
+// fruits[1] = "avocado"
+// console.log(fruits)
+// fruits.push("banana")
+// console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+// //looping through arrays
+// fruits.forEach((fruit) => {
 
+// }
+
+// )
+//     console.log(fruit)
+// let numbers =b [1,2,3,4,5,6,7,8,9,10];
+// let evennembers = numbers.filter((number)=>{
+//     return number% 2===0;
+
+let artists = {
+    artistsName: "james",
+    age :30,
+    song: "the night is still young"}
+   
+console.log(artists.artistsName)
+Object.values(artists).forEach(value => console.log(value))
+console.log(Object.entries(artists))
+for (const [key,value] of Object.entries(artists))
+
+{
+    console.log(`key : ${key} value : ${value}`)
 }
-
-)
-    console.log(fruit)
-let numbers =b [1,2,3,4,5,6,7,8,9,10];
-let evennembers = numbers.filter((number)=>{
-    return number% 2===0;
-
-}
-
-)
--- dom_excercise/index.js
+++ dom_excercise/index.js
@@ -1,19 +1,19 @@
-let el = document.querySelector("body");
+// let el = document.querySelector("body");
 
-el.classList.add("container");
-let el2 = document.querySelector("button");
-el2.classList.add("btn");
-let el3 = document.querySelector("input");
-el3.classList.add("in")
-let el4 = document.querySelector(button)
-el4.addEventListener('click',function()){
-  let userName = el3.ariaValueMax
-let p = document.createElement("h2");
-let greeting = 'Hello today is thursday';
-p.innerText= greeting
-document.body.appendChild(p) 
+// el.classList.add("container");
+// let el2 = document.querySelector("button");
+// el2.classList.add("btn");
+// let el3 = document.querySelector("input");
+// el3.classList.add("in")
+// let el4 = document.querySelector(button)
+// el4.addEventListener('click',function()){
+//   let userName = el3.ariaValueMax
+// let p = document.createElement("h2");
+// let greeting = 'Hello today is thursday';
+// p.innerText= greeting
+// document.body.appendChild(p) 
  
-}
+// }
 
 
 
@@ -22,12 +22,12 @@ document.body.appendChild(p)
 
 
 
-let userName = el3.ariaValueMax
-let p = document.createElement("h2");
-let greeting = 'Hello today is thursday';
-p.innerText= greeting
-document.body.appendChild(p)
-
+// let userName = el3.ariaValueMax
+// let p = document.createElement("h2");
+// let greeting = 'Hello today is thursday';
+// p.innerText= greeting
+// document.body.appendChild(p)
+let
 
 
 
